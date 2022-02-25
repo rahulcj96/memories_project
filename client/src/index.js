09d16f9242e65cdcc0259cacb4369bd34ec84138
rahulcj96 <rahulr1996@gmail.com>
2022-02-25 11:27:06 +0530

    client side init

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
+ReactDOM.render(<App  />, document.getElementById('root'));
