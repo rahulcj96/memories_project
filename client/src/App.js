7c900e871eca8d28970cba6cf212c69f204df1d9
rahulcj96 <rahulr1996@gmail.com>
2022-03-02 21:49:15 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from './actions/posts';
